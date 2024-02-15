e3be725410b4aea58533b65ddc455ba57a00dad4
emad <assimad8@gmail.com>
2024-02-15 22:25:24 +0100

    no change

--- Calculator.java
+++ Calculator.java
@@ -2,13 +2,7 @@ public class Calculator() {
     public int addition(int a,int b) {
         return a + b; 
     }
-<<<<<<< HEAD
-
     public double addition(double a,double b) {
         return a + b;
-=======
-    public double addition(double a,double b) {
-        return a + b;
->>>>>>> feature/addition
     }
 }
